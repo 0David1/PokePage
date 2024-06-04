c73efc969c160d236546d39f55db9606f153c072
Enzo <enzobautista1000@gmail.com>
2024-06-03 23:21:53 -0300

    s

--- vue.config.js
+++ vue.config.js
@@ -14,7 +14,7 @@ module.exports = {
       new CopyWebpackPlugin({
         patterns: [
           {
-            from: '_redirects', // Ruta del archivo _redirects en el directorio raíz del proyecto
+            from: 'src/_redirects', // Ruta del archivo _redirects en el directorio raíz del proyecto
             to: '', // Ruta de destino en el directorio de salida (por defecto es el directorio raíz)
             toType: 'dir', // Tipo de destino (directorio)
           },
