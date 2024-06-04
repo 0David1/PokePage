53394a42c3fe3f4901e0f083504faf6767f7cb65
Enzo <enzobautista1000@gmail.com>
2024-06-03 22:53:21 -0300

    fix routes reload

--- vue.config.js
+++ vue.config.js
@@ -4,3 +4,22 @@ module.exports = defineConfig({
   transpileDependencies: true,
   publicPath: process.env.NODE_ENV === 'production' ? '/' : '/'
 })
+
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  // Otras opciones de configuración...
+  configureWebpack: {
+    plugins: [
+      new CopyWebpackPlugin({
+        patterns: [
+          {
+            from: '_redirects', // Ruta del archivo _redirects en el directorio raíz del proyecto
+            to: '', // Ruta de destino en el directorio de salida (por defecto es el directorio raíz)
+            toType: 'dir', // Tipo de destino (directorio)
+          },
+        ],
+      }),
+    ],
+  },
+};
