c0071e63d24923c98492ca7e614f216b0b975c0d
Enzo <enzobautista1000@gmail.com>
2024-06-03 22:48:51 -0300

    fix routes reload

--- vue.config.js
+++ vue.config.js
@@ -4,3 +4,21 @@ module.exports = defineConfig({
   transpileDependencies: true,
   publicPath: process.env.NODE_ENV === 'production' ? '/' : '/'
 })
+
+const path = require('path');
+
+module.exports = {
+  // Otras opciones de configuración...
+  chainWebpack: config => {
+    config
+      .plugin('copy')
+      .tap(args => {
+        args[0].push({
+          from: path.resolve(__dirname, '_redirects'),
+          to: path.resolve(__dirname, 'dist'),
+          toType: 'dir'
+        });
+        return args;
+      });
+  }
+};
